fde4b0b9184288e363e8c56c540d248a1f81140d
Kai Hao <kevin830726@gmail.com>
2017-07-10 10:33:43 +0800

    add slack-button

--- packages/dailyjack-api/index.js
+++ packages/dailyjack-api/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const ApiBuilder = require('claudia-api-builder');
 const dailyjack = require('dailyjack-core');
 
@@ -16,6 +17,21 @@ const slackMessageBuilder = (...jacks) => ({
   attachments: jacks.map(jack => ({
     title: jack.title,
     text: (jack.contents || []).join('\n'),
+    callback_id: jack.id,
+    actions: [
+      {
+        name: 'rate',
+        text: ':thumbsup:',
+        type: 'button',
+        value: '1',
+      },
+      {
+        name: 'rate',
+        text: ':thumbsdown:',
+        type: 'button',
+        value: '-1',
+      },
+    ],
     footer: `dailyjack #${jack.id}`,
     footer_icon: 'http://i.imgur.com/19yCcOJ.jpg',
   })),
@@ -36,4 +52,28 @@ api.post('/slack', (request) => {
   return slackMessageBuilder(dailyjack.random());
 });
 
+api.post('/slack-button', (request) => {
+  const { callback_id, actions, original_message } = JSON.parse(request.post.payload);
+
+  const jack = original_message.attachments.find(attachment => attachment.callback_id === callback_id);
+
+  actions.forEach((action) => {
+    const index = jack.actions.findIndex(a => a.name === action.name && a.value === action.value);
+    const responseAction = jack.actions[index];
+    console.log(responseAction);
+    const group = responseAction.text.split(' ');
+
+    if (group.length < 2) {
+      responseAction.text += ' 1';
+    } else {
+      group[1] = parseInt(group[1], 10) + 1;
+      responseAction.text = group.join(' ');
+    }
+  });
+  console.log(original_message);
+
+  return original_message;
+});
+
 module.exports = api;
+/* eslint-enable camelcase */
