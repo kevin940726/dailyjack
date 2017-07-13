fd05e161ef7fb56441ee859c36ec34f953955226
Kai Hao <kevin940726@users.noreply.github.com>
2017-07-13 22:17:44 +0800

    Adopt firebase as backend (#9)
    * use firebase as db
    
    * use firebase as db
    
    * connect to firebase

--- packages/dailyjack-api/index.js
+++ packages/dailyjack-api/index.js
@@ -1,6 +1,18 @@
 /* eslint-disable camelcase */
 const ApiBuilder = require('claudia-api-builder');
-const dailyjack = require('dailyjack-core');
+const dailyjackCore = require('dailyjack-core').default;
+
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+} = process.env;
+
+const dailyjack = dailyjackCore({
+  projectId: FIREBASE_PROJECT_ID,
+  clientEmail: FIREBASE_CLIENT_EMAIL,
+  privateKey: FIREBASE_PRIVATE_KEY,
+});
 
 const api = new ApiBuilder();
 
@@ -21,18 +33,14 @@ const slackMessageBuilder = (...jacks) => ({
     actions: [
       {
         name: 'rate',
-        text: ':thumbsup:',
+        text: [':thumbsup:', Object.keys(jack.ratedUsers || {}).length]
+          .filter(Boolean)
+          .join(' '),
         type: 'button',
         value: '1',
       },
-      {
-        name: 'rate',
-        text: ':thumbsdown:',
-        type: 'button',
-        value: '-1',
-      },
     ],
-    footer: `dailyjack #${jack.id}`,
+    footer: `dailyjack #${jack.id} @${jack.author}`,
     footer_icon: 'http://i.imgur.com/19yCcOJ.jpg',
   })),
 });
@@ -43,40 +51,43 @@ api.post('/slack', (request) => {
   if (body.channel_name.indexOf('all-') === 0) {
     return;
   }
-  
+
   if (body.text === 'all') {
-    return slackMessageBuilder(...dailyjack.all());
+    return dailyjack.all()
+      .then(all => slackMessageBuilder(...all));
   }
 
   const id = parseInt(body.text, 10);
   if (Number.isInteger(id)) {
-    return slackMessageBuilder(dailyjack.get(id));
+    return dailyjack.get(id)
+      .then(slackMessageBuilder);
   }
 
-  return slackMessageBuilder(dailyjack.random());
+  return dailyjack.random()
+    .then(slackMessageBuilder);
 });
 
 api.post('/slack-button', (request) => {
-  const { callback_id, actions, original_message } = JSON.parse(request.post.payload);
+  const { callback_id, actions, user, original_message } = JSON.parse(request.post.payload);
 
   const jack = original_message.attachments.find(attachment => attachment.callback_id === callback_id);
 
-  actions.forEach((action) => {
-    const index = jack.actions.findIndex(a => a.name === action.name && a.value === action.value);
-    const responseAction = jack.actions[index];
-    console.log(responseAction);
-    const group = responseAction.text.split(' ');
-
-    if (group.length < 2) {
-      responseAction.text += ' 1';
-    } else {
-      group[1] = parseInt(group[1], 10) + 1;
-      responseAction.text = group.join(' ');
+  const actionsPromise = actions.map((action, index) => {
+    if (action.name === 'rate') {
+      return dailyjack.togglevote(callback_id, user.name)
+        .then(() => dailyjack.getRate(callback_id))
+        .then((rate) => {
+          jack.actions[index].text = [':thumbsup:', rate]
+            .filter(Boolean)
+            .join(' ');
+        });
     }
-  });
-  console.log(original_message);
 
-  return original_message;
+    return Promise.resolve();
+  });
+
+  return Promise.all(actionsPromise)
+    .then(() => original_message);
 });
 
 module.exports = api;
--- packages/dailyjack-core/index.js
+++ packages/dailyjack-core/index.js
@@ -1,9 +1,105 @@
-const jacks = require('./jacks.json');
+const admin = require('firebase-admin');
+
+const DATABASE_URL = 'https://dailyjack-8a930.firebaseio.com';
+
+const jackDB = (config = {}) => {
+  const app = admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: config.projectId,
+      clientEmail: config.clientEmail,
+      privateKey: config.privateKey,
+    }),
+    databaseURL: DATABASE_URL,
+  });
+
+  const db = admin.database();
+  const jacksRef = db.ref('jacks');
+  const totalJacksRef = db.ref('totalJacks');
+
+  const insert = (jack) => (
+    jacksRef
+      .child(jack.id)
+      .set({
+        id: jack.id,
+        title: jack.title,
+        contents: jack.contents,
+        arthor: jack.arthor,
+        ratedUsers: [],
+        createdTime: Date.now(),
+        isLimited: false,
+      })
+      .then(() => (
+        totalJacksRef.transaction(total => (total || 0) + 1)
+      ))
+  );
+
+  const all = () => (
+    jacksRef.once('value')
+      .then(snapshot => snapshot.val())
+      .then(jacks => jacks.filter(Boolean))
+  );
+
+  const get = id => (
+    jacksRef.child(id)
+      .once('value')
+      .then(snapshot => snapshot.val())
+  );
+
+  const random = () => (
+    totalJacksRef.once('value')
+      .then(snapshot => snapshot.val())
+      .then(total => get(Math.floor(Math.random() * total) + 1))
+  );
+
+  const upvote = (id, user) => (
+    jacksRef.child(id)
+      .child('ratedUsers')
+      .child(user)
+      .set(true)
+  );
+
+  const downvote = (id, user) => (
+    jacksRef.child(id)
+      .child('ratedUsers')
+      .remove(user)
+  );
+
+  const togglevote = (id, user) => (
+    jacksRef.child(id)
+      .child('ratedUsers')
+      .child(user)
+      .transaction(rate => (rate ? null : true))
+  );
+
+  const getRate = id => (
+    jacksRef.child(id)
+      .child('ratedUsers')
+      .once('value')
+      .then(snapshot => snapshot.val())
+      .then(rate => Object.keys(rate || {})
+        .filter(Boolean)
+        .length
+      )
+  );
+
+  const exit = () => {
+    db.goOffline();
+    app.delete();
+  };
+
+  return {
+    all,
+    get,
+    random,
+    insert,
+    exit,
+    upvote,
+    downvote,
+    togglevote,
+    getRate,
+  };
+};
 
 module.exports = {
-  default: jacks,
-  all: () => jacks,
-  random: () => jacks[Math.floor(Math.random() * jacks.length)],
-  get: id => jacks[id - 1],
-  size: () => jacks.length,
+  default: jackDB,
 };
