a9e1d9126440eef1452ddd4cf9ac22d60f501687
Kai Hao <kevin830726@gmail.com>
2017-08-02 13:51:37 +0800

    fix today time

--- packages/dailyjack-api/utils.js
+++ packages/dailyjack-api/utils.js
@@ -7,7 +7,13 @@ exports.isSameDay = (date1, date2 = new Date()) => (
 );
 
 exports.getToday = () => {
-  const today = spacetime(Date.now(), 'Asia/Taipei');
+  const now = new Date();
+  now.setHours(0);
+  now.setMinutes(0);
+  now.setSeconds(0);
+  now.setMilliseconds(0);
+
+  const today = spacetime(now.getTime(), 'Asia/Taipei');
 
   return today;
 };
